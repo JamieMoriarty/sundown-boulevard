e022638fdf008dd31c856381d1e28cd07698c552
Martin <martin.doesnt.exist@gmail.com>
2021-02-27 15:41:39 +0100

    Fetch data from actual API

--- src/components/Drinks/index.js
+++ src/components/Drinks/index.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Button from "../../modules/Button/index";
 
 import styles from "./Drinks.module.scss";
-import mockData from "./mockdata.json";
 
 const DrinksScreen = () => {
+  const [apiData, setApiData] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(undefined);
   const [selectedDrinks, setSelectedDrinks] = useState([]);
 
   const drinksIndex = (id) => selectedDrinks.findIndex((drinkId) => drinkId === id);
@@ -17,6 +19,32 @@ const DrinksScreen = () => {
     setSelectedDrinks(updatedDrinksList);
   };
 
+  useEffect(() => {
+    let dataCanBeSet = true;
+    const fetchDrinks = async () => {
+      setApiData(undefined);
+      setLoading(true);
+      setError(undefined);
+      let jsonResponse = undefined;
+
+      try {
+        const res = await fetch("https://api.punkapi.com/v2/beers");
+        jsonResponse = await res.json();
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+
+      if (dataCanBeSet && jsonResponse) {
+        setApiData(jsonResponse);
+      }
+    };
+
+    fetchDrinks();
+    return () => (dataCanBeSet = false);
+  }, []);
+
   const canProceed = selectedDrinks.length > 0;
 
   return (
@@ -24,17 +52,20 @@ const DrinksScreen = () => {
       <div className="row">
         <div className="col-8">
           <div className="row">
-            {mockData.map((beer) => (
-              <div className="col-6" key={beer.id}>
-                <div
-                  className={`${styles["drinks-screen__beer"]} ${isSelected(beer.id) ? styles["drinks-screen__beer--selected"] : ""}`}
-                  onClick={() => toggleDrinkSelected(beer.id)}
-                >
-                  <img src={beer.image_url} alt="" className={styles["drinks-screen__beer__image"]} height="130" />
-                  <p className={styles["drinks-screen__beer__name"]}>{beer.name}</p>
+            {!loading &&
+              !error &&
+              apiData &&
+              apiData.map((beer) => (
+                <div className="col-6" key={beer.id}>
+                  <div
+                    className={`${styles["drinks-screen__beer"]} ${isSelected(beer.id) ? styles["drinks-screen__beer--selected"] : ""}`}
+                    onClick={() => toggleDrinkSelected(beer.id)}
+                  >
+                    <img src={beer.image_url} alt="" className={styles["drinks-screen__beer__image"]} height="130" />
+                    <p className={styles["drinks-screen__beer__name"]}>{beer.name}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
         <div className="col-4">
